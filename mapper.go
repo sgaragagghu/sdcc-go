d28f466716b7498680bf5c1a9903986034cb5605
Marco Scarlino <sgaragagghu@gmail.com>
2022-10-02 16:26:03 +0000

    rpc client side

--- mapper.go
+++ mapper.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
 //	"os"
-//	"time"
-//	"log"
+	"time"
+	"net/rpc"
 )
 
 func mapper_main() {
 
+	// connect to server via rpc tcp
+	client, err := rpc.Dial("tcp", ":1200")
+	defer client.Close()
+	if err != nil {
+		ErrorLoggerPtr.Fatal(err)
+	}
+
+	// reply is used to hold responses
+	var reply int
+
+	// RPC call (1)
+	// remote call procedure Person.FirstLast (FirstLast method on Person object)
+	err = client.Call("Master_handler.Send_heartbeat", &Server{"", "", "", time.Now(), ""}, &reply)
+	if err != nil {
+		ErrorLoggerPtr.Fatal("Person.FirstLast error:", err)
+	}
+	fmt.Printf("Response: %d\n", reply)
 
 }
 
