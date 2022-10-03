50eef112a150d16d899c267433d53ec336e305f5
Marco Scarlino <sgaragagghu@gmail.com>
2022-10-03 17:52:12 +0000

    adding heartbeat goroutine

--- master.go
+++ master.go
@@ -1,16 +1,52 @@
 package main
 
 import (
-//	"fmt"
-//	"os"
-//	"time"
-//	"log"
 	"net"
 	"net/rpc"
 	"reflect"
+
+	"github.com/elliotchance/orderedmap"
 )
 
+var (
+	Heartbeat_channel_ptr *<-chan Server
+)
+
+func heartbeat_goroutine() {
+
+	InfoLoggerPtr.Println("Heartbeat_goroutine started.")
+
+	linked_hashmap := orderedmap.NewOrderedMap()
+
+	for {
+		select {
+		case heartbeat := <-*Heartbeat_channel_ptr:
+			element_temp := linked_hashmap.GetElement(heartbeat.Id)
+			var server_temp_ptr *Server
+			if element_temp != nil {
+				server_temp_ptr = element_temp.Value.(*Server)
+				server_temp_ptr.Last_heartbeat = heartbeat.Last_heartbeat
+				linked_hashmap.Delete(heartbeat.Id) // TODO check error (COPIARE PRIMA DI ELIMINARE.... memcp)
+			} else {
+				// TODO build the server...
+				server_temp_ptr.Last_heartbeat = heartbeat.Last_heartbeat
+			}
+			linked_hashmap.Set(server_temp_ptr.Id, *server_temp_ptr) // TODO check error (e anche controllare che non serve in realta' il puntatore..) 
+			InfoLoggerPtr.Println("received heartbeat")
+		default:
+		}
+
+		// TODO controllare le scadenze
+	}
+}
+
 func master_main() {
+
+	// creating channel for communicating the heartbeat
+	// to the goroutine heartbeat manager
+	Heartbeat_channel_ptr = new(<-chan Server)
+
+	go heartbeat_goroutine()
 
 	master_handler := new(Master_handler)
 
